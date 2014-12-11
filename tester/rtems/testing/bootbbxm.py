c9b7dbf0198dfecc0dfab7a7efeff3c025d3227e
Brandon Matthews <bmatt@optimaltour.us>
2014-12-10 21:51:58 -0800

    Docstrings are good.

--- tester/rtems/testing/bootbbxm.py
+++ tester/rtems/testing/bootbbxm.py
@@ -7,14 +7,18 @@ import subprocess
 import shutil
 
 def parseargs():
+    """
+    Parse and validate arguments to the program. We do basic, but not
+    exhaustive, sanity checking. 
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("rtdir", help="Path of RTEMS builder directory (~/dev/rtems)")
     parser.add_argument("exefile", help="Full path to built executable (.../hello/hello.exe)")
+    parser.add_argument("-o", "--outfile", help="Destination to copy disk image file after creation")
+    parser.add_argument("-x", "--execute", action="store_true", help="Start qemu after building image")
     parser.add_argument("-b", "--bsize", type=int, default=512, help="Size of disk img block size in bytes")
     parser.add_argument("-s", "--imgsize", type=int, default=30, help="Size of the disk image in MiB")
     parser.add_argument("-t", "--tmpdir", help="Temporary directory to store working files")
-    parser.add_argument("-o", "--outfile", help="Disk image file to write")
-    parser.add_argument("-x", "--execute", action="store_true", help="Start qemu after building image")
     args = parser.parse_args()
 
     # Sanity check of required arguments
@@ -39,8 +43,12 @@ def parseargs():
 
     return args
 
-# Procedural programming at its finest.
 def build_image(rtdir, exefile, bsize, imgsize, tmpdir):
+    """
+    Perform the steps necessary to create a blank image, and then populate
+    it. Because we create a bare image, and also a FAT image, the disk space
+    required is approximately 2*imgsize.
+    """
     img_fn   = os.path.join(tmpdir, "bbxm_boot_sdcard.img")
     fat_fn   = os.path.join(tmpdir, "bbxm_boot_fat.img")
     app_name = "rtems_app.img"
@@ -68,6 +76,10 @@ def build_image(rtdir, exefile, bsize, imgsize, tmpdir):
     return img_fn
 
 def zero_img(img_fn, bsize, seek):
+    """
+    Use dd to create a blank (all zeroes) file. To avoid actually writing zeroes,
+    we use seek to skip to the last block.
+    """
     try:
         subprocess.check_output([ 'dd', 
             'if=/dev/zero', 
@@ -80,6 +92,9 @@ def zero_img(img_fn, bsize, seek):
         raise RuntimeError("writing file {} failed with error {}: {}".format(img_fn, e.retcode, e.output))
 
 def format_fat(fat_fn, bsize, fs_size):
+    """
+    Use BSD's newfs_msdos to write a FAT filesystem onto the specified file.
+    """
     try:
         subprocess.check_output([ os.path.join(rtdir, "bin", "newfs_msdos"),
             '-r', '1', '-m', '0xf8', '-c', '4', '-F16', '-h', '64', '-u', '32', '-o', '0',
@@ -90,6 +105,13 @@ def format_fat(fat_fn, bsize, fs_size):
         raise RuntimeError("formatting image {} failed with error {}: {}".format(fat_fn, e.retcode, e.output))
 
 def prep_exe(rtdir, exefile, tmpdir, app_fn):
+    """
+    Use GNU objcopy to turn an ELF relocatable binary (".exe") into a bare
+    binary, and gzip it. Then, use U-Boot's mkimage helper to turn that bare
+    binary into a "U-Boot image", destined for memory address 0x80000000.
+
+    We leave this transformed binary in the working directory, and do not copy it out.
+    """
     exebase = os.path.basename(exefile)
     binfile = os.path.join(tmpdir, exebase + ".bin")
     try:
@@ -104,6 +126,7 @@ def prep_exe(rtdir, exefile, tmpdir, app_fn):
         raise RuntimeError("failed to prep exe: {}".format(repr(e)))
 
 def copy_file_to_fat(rtdir, fatimg, copy_fn):
+    """ Copy the specified file into the root of the specified FAT disk image file. """
     copy_base = os.path.basename(copy_fn)
     try:
         subprocess.check_output([ os.path.join(rtdir, "bin", "mcopy"),
@@ -112,6 +135,7 @@ def copy_file_to_fat(rtdir, fatimg, copy_fn):
         raise RuntimeError("failed to copy file {} to image {}: {}".format(copy_fn, fatimg, repr(e)))
 
 def partition_img(rtdir, sd_img, offset):
+    """ Create a partition table with a single entry in the specified image file """
     try:
         subprocess.check_output([ os.path.join(rtdir, "bin", "partition"),
             '-m', sd_img, offset, 'c:0*+' ])
@@ -119,6 +143,10 @@ def partition_img(rtdir, sd_img, offset):
         raise RuntimeError("failed to partition image: {}".format(repr(e)))
 
 def copy_fat_to_img(sd_img, fat_img, offset):
+    """ 
+    Directly write the specified FAT disk image file onto the larger SD card
+    image file, offset by a sufficient amount.
+    """
     try:
         subprocess.check_output([ 'dd',
             'of={}'.format(sd_img), 'if={}'.format(fat_img), 'seek={}'.format(offset) ])
@@ -126,23 +154,29 @@ def copy_fat_to_img(sd_img, fat_img, offset):
         raise RuntimeError("failed to copy fat image: {}".format(repr(e)))
 
 def main():
+    # Gather information from the command line, doing basic sanity checking.
     args = parseargs()
 
-    # If we weren't handed a temp dir, create one
+    # If we weren't handed a temp dir, create one.
     if 'tmpdir' not in args:
         import tempfile
         args.tmpdir = tempfile.mkdtemp()
 
+    # Create the disk image within the temp directory.
     outimg = build_image(args.rtdir, args.exefile, args.bsize, args.imgsize, args.tmpdir)
 
+    # Copy the image file out of the temp directory if requested.
     if "outfile" in args:
         shutil.copyfile(outimg, args.outfile)
 
+    # Invoke qemu-linaro's qemu-system-arm program to boot the newly created SD card image
+    # TODO: Make this appropriate/flexible for the various BB* targets.
     if args.execute:
         subprocess.call([ os.path.join(args.rtdir, "qemu-linaro", "bin", "qemu-system-arm"),
             '-display', 'none', '-serial', 'stdio', '-M', 'beaglexm', '-no-reboot',
             '-monitor', 'none', '-nographic', '-sd', outimg])
 
+    # Delete the working directory.
     shutil.rmtree(args.tmpdir)
 
 if __name__=="__main__":
